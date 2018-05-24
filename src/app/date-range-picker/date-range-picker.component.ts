f6b7e9d7202660dfe63a087c02e2230fff69f4ad
[henry hamamji] <[hhamamji@samiro.com]>
2018-05-24 12:45:00 -0600

    calendar logic in progress

--- src/app/date-range-picker/date-range-picker.component.ts
+++ src/app/date-range-picker/date-range-picker.component.ts
@@ -12,6 +12,8 @@ export class DateRangePickerComponent implements OnInit {
   public daysArray;
   public dateFrom;
   public dateTo;
+  public dateConsidered;
+  public currentDate = moment();
 
   constructor() { }
 
@@ -21,7 +23,7 @@ export class DateRangePickerComponent implements OnInit {
 
   public createCalendar(month) {
     let firstDay = moment(month).startOf('M');
-    let days = Array.apply(null, { length: month.daysInMonth()})
+    let days = Array.apply(null, { length: month.daysInMonth() })
       .map(Number.call, Number)
       .map(n => {
         return moment(firstDay).add(n, 'd');
@@ -33,66 +35,104 @@ export class DateRangePickerComponent implements OnInit {
 
   }
 
-public todayCheck(day){
-  if(!day){
-    return false;
+  public todayCheck(day) {
+    if (!day) {
+      return false;
+    }
+    return moment().format('L') === day.format('L');
   }
-  return moment().format('L') === day.format('L');
-}
 
-public nextMonth(){
-this.date.add(1, 'M');
-this.daysArray = this.createCalendar(this.date);
-}
+  public nextMonth() {
+    this.date.add(1, 'M');
+    this.daysArray = this.createCalendar(this.date);
+  }
 
 
-public previousMonth()
-{
-  this.date.subtract(1, 'M');
-this.daysArray = this.createCalendar(this.date);
+  public previousMonth() {
+    this.date.subtract(1, 'M');
+    this.daysArray = this.createCalendar(this.date);
 
-}
+  }
 
-public selectedDate(day){
-  let dayFormatted = day.format('MM/DD/YYYY');
-  if(this.isFormValid())
-  {
-    this.dateTo = null;
-    this.dateFrom = null;
+  public selectedDate(day) {
+    if (!day  || this.isDayInthePast(day)) {
+      return;
+    }
+    let dayFormatted = day.format('MM/DD/YYYY');
+
+    if (this.isFormValid()) {
+      this.dateTo = null;
+      this.dateFrom = null;
       console.log("date from: " + this.dateFrom);
-  console.log("date to: " + this.dateTo);
-    return;
-  }
-  if(this.dateFrom == null){
-    this.dateFrom = dayFormatted;
-  }
-  else{
-    this.dateTo = dayFormatted;
-  }
-  console.log("date from: " + this.dateFrom);
-  console.log("date to: " + this.dateTo);
+      console.log("date to: " + this.dateTo);
+      return;
+    }
+    if (this.dateFrom == null) {
+      this.dateFrom = dayFormatted;
+    }
+    else {
+      this.dateTo = dayFormatted;
+    }
+    console.log("date from: " + this.dateFrom);
+    console.log("date to: " + this.dateTo);
 
-}
-
-public isFormValid(){
-  if(this.dateFrom != null && this.dateTo != null){
-    return true;
   }
-  return false;
-}
 
-public isSelected(day){
-  let dateFrom = moment(this.dateFrom, 'MM/DD/YYYY');
-  let dateTo = moment(this.dateTo, 'MM/DD/YYYY');
-  if(!day){
+  public isFormValid() {
+    if (this.dateFrom != null && this.dateTo != null) {
+      return true;
+    }
     return false;
   }
-  if(this.isFormValid()){
-    return dateFrom.isSameOrBefore(day) && dateTo.isSameOrAfter(day);
+
+  public isSelected(day) {
+    let dateFrom = moment(this.dateFrom, 'MM/DD/YYYY');
+    let dateTo = moment(this.dateTo, 'MM/DD/YYYY');
+    if (!day) {
+      return false;
+    }
+    if (this.isFormValid()) {
+      return dateFrom.isSameOrBefore(day) && dateTo.isSameOrAfter(day);
+    }
+    if (this.dateFrom != null) {
+      return dateFrom.isSame(day);
+    }
   }
-  if(this.dateFrom!=null){
-    return dateFrom.isSame(day);
+
+  isConsidered(day) {
+    let dateFrom = moment(this.dateFrom, 'MM/DD/YYYY');
+    let dateTo = moment(this.dateTo, 'MM/DD/YYYY');
+    let dateConsidered = moment(this.dateConsidered, 'MM/DD/YYYY');
+
+    if (!day || this.isFormValid()) {
+      return false;
+    }
+    if (this.dateFrom == null) {
+      return false;
+    }
+    else {
+      return dateFrom.isSameOrBefore(day) && dateConsidered.isSameOrAfter(day);
+    }
   }
-}
+
+  getDateMousedOver(day) {
+    if (!day || this.isDayInthePast(day)) {
+      return false;
+    }
+    let dayFormatted = day.format('MM/DD/YYYY');
+    this.dateConsidered = dayFormatted;
+    return day;
+  }
+
+  isDayInthePast(day){
+    //console.log(this.currentDate.format('MM/DD/YYYY'));
+    //console.log(day.format('MM/DD/YYYY'));
+
+    if(day.isBefore(this.currentDate, 'day')){
+      return true;
+    }
+    return false;
+  }
+
 
 }
